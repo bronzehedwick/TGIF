fb629765261b75cca6d454e34d701c9ced221e0b
B-rye <brianmanhard@gmail.com>
2020-08-14 15:41:19 -0400

    Passing user tests commit

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,21 +1,14 @@
-require 'rails_helper'
-
 feature 'user registers', %Q{
   As a visitor
   I want to register
   So that I can create an account
 } do
 
-  # Acceptance Criteria:
-  # * I must specify a valid email address,
-  #   password, and password confirmation
-  # * If I don't specify the required information, I am presented with
-  #   an error message
-
   scenario 'provide valid registration information' do
     visit new_user_registration_path
 
     fill_in 'Email', with: 'john@example.com'
+    fill_in 'Username', with: 'johnnyjohn123'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
 
